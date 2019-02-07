6ba2c8c261724f68c9739ca886e76ce5a85eed27
xfry <fry.andrade@gmail.com>
2019-02-07 20:38:12 +0000

    fixing size of file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
 	printf("Size of a char: %d bytes(s)\n", sizeof(char));
 	printf("Size of an int: %d bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %d bytes(s)\n", sizeof(float));
-	return (0);
+	return(0);
 }
