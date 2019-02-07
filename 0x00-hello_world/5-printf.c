c0440ab06eba91031abecb75a05b928086d7eee1
xfry <fry.andrade@gmail.com>
2019-02-07 19:25:43 +0000

    adding comment before function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*This function will execute the next process*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
