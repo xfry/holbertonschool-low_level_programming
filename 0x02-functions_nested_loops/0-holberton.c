c2c3f5388dddf2ce6f40134168a7f82326820ef7
xfry <fry.andrade@gmail.com>
2019-02-11 23:01:11 +0000

    following betty doc to implment good comments for every function

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
 #include "unistd.h"
 /**
- * main - Entry point
+ * print_message - print the string holberton
+ * @text: store the text to print
  *
- * Return: Always 0 (Success)
+ * holberton.h - header with function prototypes
+ * Return: void
  */
 void print_message(char *text)
 {
 	write(1, text, sizeof(text) + 2);
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	print_message("Holberton\n");
