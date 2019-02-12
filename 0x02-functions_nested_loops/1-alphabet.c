d939f287a855bca34f87f9c9a5fd2b98ccda0fa9
xfry <fry.andrade@gmail.com>
2019-02-12 14:50:58 +0000

    added new program to generate alphabet, modified holberton.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include "_putchar.c"
+/**
+ * generate_alphabet - a loop to generate a-z text
+ *
+ * description: this function generate the alphabet
+ * Return: void
+ */
+void generate_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar (letter);
+
+	_putchar('\n');
+}
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	generate_alphabet();
+	return (0);
+}
