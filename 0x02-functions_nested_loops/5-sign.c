c303e825099156deec02e0be6376e987a50c9875
xfry <fry.andrade@gmail.com>
2019-02-12 20:02:26 +0000

    a function to print sign and value

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_sign a function that prints sign of a integer
+ * @n: a integer number
+ *
+ * Return: integer value
+ */
+int print_sign(int n)
+{
+	char sign;
+	int rvalue;
+
+	if(n == 0)
+	{
+		_putchar('0');
+		return 0;
+	}
+
+	sign = n > 0 ? '+' : '-';
+	_putchar(sign);
+	rvalue = n > 0 ? 1 : -1;
+	return (rvalue);
+}
