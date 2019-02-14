3adb895f47ea6f97ddefc69466e0792a11c2967c
xfry <fry.andrade@gmail.com>
2019-02-14 22:24:26 +0000

    a function that return 0 or 1 if the character received is a uppercase

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
-
+/**
+ * _isupper - return 0 or 1
+ * @character: a in character
+ *
+ * description: a function that return 0 or 1
+ * if the character is uppercase
+ * Return: int value about the character
+ */
 int _isupper(int character)
 {
-	return (character > 64 && character <=90 ? 1 : 0);
+	return (character > 64 && character <= 90 ? 1 : 0);
 }
