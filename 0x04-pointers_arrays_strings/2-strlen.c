f03615cc9c58f6822c1c976d92ac2d7344b8e0bd
xfry <fry.andrade@gmail.com>
2019-02-20 23:20:40 +0000

    print the length of a string

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include "_putchar.c"
+/**
+ * _strlen - A function that return string length
+ * @s: a char pointer
+ *
+ * description: A function that returns a string length
+ * Return: an integer value
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
