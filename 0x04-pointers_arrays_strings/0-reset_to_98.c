52d190517478c704a4438667095ffed24b175c1d
xfry <fry.andrade@gmail.com>
2019-02-20 22:10:02 +0000

    added new program to change the value through a pointer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - a function to change a value by pointer
+ * @*n: an integer as pointer
+ *
+ * description: This function receive a value and change it.
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
