8ce73427f6471b7855cd9ed233883499be0d4883
xfry <fry.andrade@gmail.com>
2019-02-21 00:01:29 +0000

    a function that prints an string at the stdoutput

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <unistd.h>
+/**
+ * _puts - print a string
+ * @str: a char pointer param
+ *
+ * description: a function that prints an string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int len = 0;
+	while(str[len] != '\0')
+	{
+		write(2, &str[len], 1);
+		len++;
+	}
+	write(2, "\n", 1);
+}
