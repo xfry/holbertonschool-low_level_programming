8005befe8ce03456c1085ff299290a6918c086ce
xfry <fry.andrade@gmail.com>
2019-02-24 23:59:12 +0000

    fix: added betty style

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,23 @@
 #include "holberton.h"
-
+/**
+ * rev_string - a function to reverse string
+ * @s: a (array decay) pointer
+ *
+ * description: This function can reverse whatever string you write
+ * Return: void, nothing to return.
+ */
 void rev_string(char *s)
 {
 	int length = 0, total_length = 0;
 	int new_length = 0;
 	char temp;
 
-	while(s[length] != '\0')
+	while (s[length] != '\0')
 		length++;
-	length -= 1;
-	total_length = length;
-	while(length > total_length*0.5)
+
+	total_length = (length -= 1);
+
+	while (length > (total_length * 0.5))
 	{
 		temp = s[new_length];
 		s[new_length] = s[length];
