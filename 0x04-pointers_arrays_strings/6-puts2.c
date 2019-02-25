17c857926882ba82f01a496301030499eacb1975
xfry <fry.andrade@gmail.com>
2019-02-25 01:47:24 +0000

    added a prototype and function to prints odd characters

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include "_putchar.c"
+/**
+ * puts2 - a function to prints odd characters
+ * @str: a char pointer param
+ *
+ * description: a function that prints letters from first
+ * out of 2 of a string.
+ * return: void, nothing
+ */
+void puts2(char *str)
+{
+	int length = 0;
+
+	do {
+		if (length == 0)
+			_putchar(str[length]);
+		else if (length % 2 == 0)
+			_putchar(str[length]);
+		length++;
+	} while (str[length] != '\0');
+	_putchar('\n');
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -7,4 +7,5 @@ int _putchar(char);
 void _puts(char *);
 void print_rev(char *);
 void rev_string(char *);
+void puts2(char *);
 #endif
