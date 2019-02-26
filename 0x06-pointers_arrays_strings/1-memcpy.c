6b589ad71282da6ef1fce05e31b219e8f424463c
xfry <fry.andrade@gmail.com>
2019-02-26 03:19:01 +0000

    added a new function to copy a memory area from src to dest

---
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _memcpy - a function to copy a memory area
+ * @dest: a char pointer to store values from s[n]
+ * @src: a char pointer
+ * @n: the unsigned integer bound of area to copy
+ *
+ * description: a function to copy a memory area into one dest
+ * Return: char pointer.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[n];
+		i++;
+	}
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,4 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
 #endif
