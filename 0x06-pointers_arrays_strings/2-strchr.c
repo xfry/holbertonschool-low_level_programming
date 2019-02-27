039b412ad38854c1aba2de2ac9ec590dee5ae485
xfry <fry.andrade@gmail.com>
2019-02-27 02:04:59 +0000

    a test

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,12 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int length = 0;
-	char arr[] = {'0'};
-	char *p = arr;
 
 	while (s[length])
 	{
 		if (s[length] == c)
 		{
-			arr[0] = c;
-			return (p);
+			return (s);
 		}
 		length++;
 	}
