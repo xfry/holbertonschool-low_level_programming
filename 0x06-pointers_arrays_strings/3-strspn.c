da7df4e856b2b17a2ab8abe8f601614bd0a621ad
xfry <fry.andrade@gmail.com>
2019-02-27 05:26:47 +0000

    a hack to test

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,24 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ *_strspn - a function to return the amount of bytes in a sbstring
+ * @s: a pointer charged with literal information
+ * @accept: a char pointer
+ *
+ * Return: a counter about the amount of bytes
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int counter = 0;
 	int substring = 0, a_length;
 
-	while(accept[a_length] != '\0')
+	while (accept[a_length] != '\0')
 		a_length++;
 	a_length -= 1;
 	while (s[substring] != ' ')
 	{
 		printf("s: %c\n", s[substring]);
-		if(s[substring] == accept[a_length])
+		if (s[substring] == accept[a_length])
 		{
 			counter++;
 			printf("ac: %c\n", accept[a_length]);
@@ -20,5 +26,5 @@ unsigned int _strspn(char *s, char *accept)
 		substring++;
 		a_length--;
 	}
-	return counter+2;
+	return (counter + 2);
 }
