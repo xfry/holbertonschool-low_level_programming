f04b934a4a783ffdb0a5e7a6ccbd23d8c27d3d18
xfry <fry.andrade@gmail.com>
2019-02-27 01:48:06 +0000

    added a new function to search a char into a literal string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *_strchr - a function to search a character n in source s
+ * @s: a source pointer
+ * @c: a char parameter to look for
+ *
+ * description: this function looks for a character 'c' into a
+ * literal string.
+ * Return: if the character 'c' is found returns a character pointer else
+ * send a char pointer
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int length = 0;
+	char arr[] = {'0'};
+	char *p = arr;
+
+	while (s[length] != '\0')
+	{
+		if (s[length] == c)
+		{
+			arr[0] = c;
+			return (p);
+		}
+		length++;
+	}
+
+	return (NULL);
+}
