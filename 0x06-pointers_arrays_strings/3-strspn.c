3b0ab24ab239981461761474a71730026dfed59c
xfry <fry.andrade@gmail.com>
2019-03-01 20:26:14 +0000

    done: a function to return the length of a substring in a source string

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *_strspn - a function to return the amount of bytes in a sbstring
  * @s: a pointer charged with literal information
@@ -9,20 +8,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int counter = 0;
-	int substring = 0, a_length;
+	unsigned int counter = 0, length = 0;
 
-	while (accept[a_length] != '\0')
-		a_length++;
-	a_length -= 1;
-	while (s[substring] != ' ')
+	while(s[length] != ' ')
 	{
-		if (s[substring] == accept[a_length])
+		if(s[length] == accept[length])
 		{
 			counter++;
+			length++;
+		} else
+		{
+			break;
 		}
-		substring++;
-		a_length--;
 	}
-	return (counter + 1);
+
+	return (counter);
 }
