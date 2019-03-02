bdf6f395d0a70dc4a78fda86995d473c67d4860f
xfry <fry.andrade@gmail.com>
2019-03-02 16:30:00 +0000

    find a character into another string and return the substring if character match

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *_strstr - a function to find the a string character into another
+ * @haystack a pointer to literal string
+ * @needle a pointer to literal string
+ *
+ * description: a function to find the a string character into another
+ * and return the substrig that match the character
+ * Return: a char character.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int length_a = 0, length_b = 0;
+
+	for (length_a = 0; needle[length_a] != '\0'; length_a++)
+	{
+		for (length_b = 0; haystack[length_b] != '\0'; length_b++)
+		{
+			if (needle[length_a] == haystack[length_b])
+			{
+				return (&haystack[length_b]);
+			}
+		}
+
+	}
+
+	return (0);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,4 +5,5 @@ char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
+char *_strstr(char *haystack, char *needle);
 #endif
