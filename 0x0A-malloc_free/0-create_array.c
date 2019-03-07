7274b1d81bd022713f303c55dafa25edba3dbaba
xfry <fry.andrade@gmail.com>
2019-03-07 20:11:48 +0000

    fix: validates case when size is less than 1

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	arr = malloc(sizeof(*arr) * size);
-	if (arr != NULL)
+	if (arr != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			arr[i] = c;
