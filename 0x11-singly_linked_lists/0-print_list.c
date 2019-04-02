4d665b250f98a4ecd24f987f605622662f9fac04
xfry <fry.andrade@gmail.com>
2019-04-02 15:44:27 +0000

    following betty's style guideline

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include "lists.h"
-
+/**
+ * print_list - a function to print a list of nodes
+ * @h: a const pointer parameter
+ *
+ * description: a function to print a list of elements for every node
+ * into a singly linked list.
+ * Return: size_t value
+ */
 size_t print_list(const list_t *h)
 {
 	size_t length;
 
-	while(h)
+	while (h)
 	{
-		if(h->str != NULL)
+		if (h->str != NULL)
 		{
 			printf("[%d] %s \n", h->len, h->str);
-
-		} else {
-			printf("[%d] %p \n", 0 , (void *)h->str);
+		}
+		else
+		{
+			printf("[%d] %p \n", 0, (void *)h->str);
 		}
 		h = h->next;
 		length++;
 	}
 
-	return length;
+	return (length);
 }
