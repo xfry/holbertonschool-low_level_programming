b61c2dec0458a0c90fa7c5428b49640ffb456567
xfry <fry.andrade@gmail.com>
2019-04-02 20:42:28 +0000

    added some betty's style code

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,12 +1,17 @@
 #include <stddef.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * list_len - a program to count the next element in list.
+ * @h: a const pointer to the head of a list.
+ *
+ * Return: a unsigned int parameter using size_t
+ */
 size_t list_len(const list_t *h)
 {
 	unsigned long int length;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		length++;
