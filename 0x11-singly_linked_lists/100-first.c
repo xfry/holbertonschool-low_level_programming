477b8616da907b061ac13151d345d9cfba558e86
xfry <fry.andrade@gmail.com>
2019-05-23 02:18:04 +0000

    delete main

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,18 +5,6 @@ void before(void) __attribute__ ((constructor));
 void after(void) __attribute__ ((constructor));
 
 /**
- * main - check the code for holberton school students.
- *
- * Return: always 0.
- */
-int main(void)
-{
-	printf("(a tortoise, having pretty good sense of a hare's nature,"
-	       " challenges one to a race.)\n");
-	return (0);
-}
-
-/**
  * before - A function to excecute before main
  * Return: void
  */
