d2f503b16471573a4a763ae03df8f2df156b9ae7
xfry <fry.andrade@gmail.com>
2019-05-23 03:29:39 +0000

    added new comments with betty style

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,25 @@
 #include<stddef.h>
 #include "lists.h"
 
+/**
+ * print_listint - a funciton to prints a linked list
+ * @h: a pointer with a constant type
+ *
+ * Description: a function that can count the amount of nodes
+ * inside a singly linked list and print his data
+ * Return: size_t value aca (a unsigned int)
+ */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *aux;
 	size_t amount = 0;
 
 	aux = h;
-	while(aux)
+	while (aux)
 	{
 		amount++;
 		printf("%d \n", aux->n);
-		if(aux->next)
+		if (aux->next)
 		{
 			aux = aux->next;
 		}
