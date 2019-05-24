613aad62e67a889772f1eb0537e07800d4536d4c
xfry <fry.andrade@gmail.com>
2019-05-24 02:53:55 +0000

    added comments

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * listint_len - a function to count the nodes of a list
+ * @h: a pointer to the head of the list
+ *
+ * Return: a unsigned int with the amount of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *aux;
