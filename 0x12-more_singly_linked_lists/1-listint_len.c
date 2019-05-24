43f0e1867c0b7ee5c16a9a65ffd5455f9cf929b1
xfry <fry.andrade@gmail.com>
2019-05-24 02:54:56 +0000

    fix code style

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -9,16 +9,17 @@ size_t listint_len(const listint_t *h)
 {
 	const listint_t *aux;
 	unsigned int counter = 0;
+
 	aux = h;
-	while(aux)
+	while (aux)
 	{
 		counter++;
-		if(aux->next)
+		if (aux->next)
 			aux = aux->next;
 		else
 		{
 			aux = NULL;
 		}
 	}
-	return counter;
+	return (counter);
 }
