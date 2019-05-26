04f4798d606790158906a0ac491044cf2e4c005e
xfry <fry.andrade@gmail.com>
2019-05-26 23:43:23 +0000

    a funciton to pop the head of a linked list

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - a function to pop the head of a linked list
+ * @head: a double pointer to the head of the list
+ *
+ * Description: a function to pop the head of a linked list
+ * Return: an integer with the data of head before pop
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *aux;
+	int n = 0;
+
+	if (head)
+	{
+		aux = *head;
+		n = aux->n;
+		aux = aux->next;
+		free(*head);
+		*head = aux;
+	}
+
+	return (n);
+}
