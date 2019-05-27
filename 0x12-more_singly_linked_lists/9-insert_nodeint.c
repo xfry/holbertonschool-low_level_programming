904763352c35b779db0ecdddb571972f81e5ffdb
xfry <fry.andrade@gmail.com>
2019-05-27 19:35:03 +0000

    return the modified head if index is 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,6 +51,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			next_node = aux->next;
 			new_node->next = next_node;
 			*head = new_node;
+			return (*head);
 		}
 		while (aux->next)
 		{
