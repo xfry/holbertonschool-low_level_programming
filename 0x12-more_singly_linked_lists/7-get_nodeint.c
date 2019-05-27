0b5716c025b9f32acd89fd2db21646da1f4f7d66
xfry <fry.andrade@gmail.com>
2019-05-27 02:12:06 +0000

    get node at index asked

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,25 @@
+#include<stdio.h>
 #include<stdlib.h>
 #include "lists.h"
+/**
+ * list_len - a program to count the next element in list.
+ * @h: a const pointer to the head of a list.
+ *
+ * Return: a unsigned int parameter using size_t
+ */
+size_t list_len(const listint_t *h)
+{
+	unsigned long int length = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		length++;
+	}
+
+	return (length);
+}
+
 /**
  * get_nodeint_at_index - a function to get a node at specific index
  * @head: a pointer to the head of the list
@@ -11,9 +31,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *aux;
 	unsigned int counter = 0;
+	if(index > list_len(head))
+		return (NULL);
 
-	if (head)
+	if (head && (index <= list_len(head)))
 	{
+		if(index == 0)
+			return (head);
 		aux = head;
 		while (aux)
 		{
