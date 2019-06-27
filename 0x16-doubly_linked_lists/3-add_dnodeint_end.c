ad142c75b387a308dcee68d7d40df2076c85d288
xfry <fry.andrade@gmail.com>
2019-06-27 23:37:47 +0000

    added a new function to add a new node at the end of the list

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint_end - a function to add a node to the end of a list
+ * @head: a double pointer parameter to the dblist head
+ * @n: a constant data value for list node
+ *
+ * description: a function to add a node to the end of the list
+ * Return: a pointer. the head of the list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *aux = NULL;
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
+	aux = *head;
+	while (aux)
+	{
+		if (!aux->next)
+		{
+			aux->next = new_node;
+			break;
+		}
+
+		aux = aux->next;
+	}
+
+	return (*head);
+}
