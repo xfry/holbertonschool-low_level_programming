07e338e7679b165a52c16449c3ed93dd6776ee78
xfry <fry.andrade@gmail.com>
2019-06-27 23:39:23 +0000

    added a new prototype for add_dnodeint_end

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -18,4 +18,6 @@ typedef struct dlistint_s
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *);
 size_t dlistint_len(const dlistint_t *);
+dlistint_t *add_dnodeint(dlistint_t **, const int);
+dlistint_t *add_dnodeint_end(dlistint_t **, const int);
 #endif
