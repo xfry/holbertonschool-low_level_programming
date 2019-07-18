b75cb437ff81e7bbe7b84a5699c1638d8f48536c
xfry <fry.andrade@gmail.com>
2019-07-17 21:52:25 -0500

    fix: betty holberton style code

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_dlistint - a function to free a double linked list.
+ * @head: a pointer to the head of the list.
+ *
+ * Description: this is a function to free memory used by a
+ * doubly linked list.
+ * Return: void.
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *aux = NULL;
