2eff11950a158e3d5d77005dde6bbd3e7fb15611
xfry <fry.andrade@gmail.com>
2019-07-18 11:22:17 -0500

    fix: set to NULL aux, if index is out of range

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - a function to get the nth node.
+ * @head: the head of the doubly list.
+ * @index: the nth index we want to get.
+ *
+ * Description: a function to get the nth node of a doubly linked list
+ * Return: it can return the nth node or null if it's out of range
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *aux = head;
 	unsigned int counter = 0;
 
-	while(counter < index)
+	while (counter < index)
 	{
-		if(aux->next)
+		if (aux->next)
 		{
 			aux = aux->next;
 		}
@@ -16,8 +22,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		{
 			return (aux);
 		}
-		counter ++;
+		counte++;
 	}
+
+	aux = NULL;
 
 	return (aux);
 }
