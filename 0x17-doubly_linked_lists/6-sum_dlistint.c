e908811b8093000ccb7c686e110e46930be5641c
xfry <fry.andrade@gmail.com>
2019-07-22 21:04:01 -0500

    fix: added comments and fix stylecode

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - a function to sum the data of every node
+ * @head: a pointer to the head of the list.
+ *
+ * Description: A function to sum all the data of every node.
+ * Return: 0 if the head node is empty, otherwise sum.
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *aux = head;
 	int sum = 0;
 
-	if(!head)
+	if (!head)
 		return (0);
 
 	while (aux)
